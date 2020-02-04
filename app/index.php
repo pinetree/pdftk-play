b20fb89856d6f80c0e236b9a899bdfd0d255c9c8
pinetree <kirill.sosnin@gmail.com>
2020-02-04 17:05:14 +0300

    Add personInfo

--- app/index.php
+++ app/index.php
@@ -3,19 +3,61 @@ require_once 'vendor/autoload.php';
 
 use mikehaertl\pdftk\Pdf;
 
-$pdf = new Pdf('/var/www/html/public/sample.pdf', [
+error_reporting(0);
+
+$data = [];
+foreach ($_POST as $k => $v) {
+    $data[$k] = json_decode(stripslashes($v), true);
+};
+
+$pdf = new Pdf('/var/www/html/public/template2.pdf', [
     'command' => '/usr/bin/pdftk',
     'useExec' => true
 ]);
 $pdf->tempDir = '/var/www/html/public/work';
 
+$name = getName($data);
+
 if (!$pdf->allow('AllFeatures')->fillForm([
-    'personInfo.lastName' => ('qqq1Соснин Кирилл2'),
-    'personInfo.nameChanged' => 'Yes',
+    'personName' => $name,
+    'personRole' => getRole($data),
+    'personInfo.lastName' => $data['personInfo']['lastName'],
+    'personInfo.firstName' => $data['personInfo']['firstName'],
+    'personInfo.patronymic' => $data['personInfo']['patronymic'],
+    'personInfo.nameChanged' => checkbox($data['personInfo']['nameChanged']),
+    'personInfo.previousLastName' => $data['personInfo']['previousLastName'],
+    'personInfo.previousFirstName' => $data['personInfo']['previousFirstName'],
+    'personInfo.previousPatronymic' => $data['personInfo']['previousPatronymic'],
+    'personInfo.nameChangedDate' => $data['personInfo']['nameChangedDate'],
+    'personInfo.birthDate' => $data['personInfo']['birthDate'],
+    'personInfo.nameChangedReason' => $data['personInfo']['nameChangedReason'],
+    'personInfo.birthPlace' => $data['personInfo']['birthPlace'],
+    'personInfo.genderMale' => checkbox($data['personInfo']['gender'] === 'male'),
+    'personInfo.genderFemale' => checkbox($data['personInfo']['gender'] === 'female'),
+    'personInfo.citizenshipRussia' => checkbox($data['personInfo']['citizenship']  === 'russia'),
+    'personInfo.citizenshipOther' => checkbox($data['personInfo']['citizenship'] === 'other'),
+    'personInfo.employmentStatus:work' => checkbox($data['personInfo']['employmentStatus'] === 'work'),
+    'personInfo.employmentStatus:army' => checkbox($data['personInfo']['employmentStatus'] === 'army'),
+    'personInfo.employmentStatus:pensioner' => checkbox($data['personInfo']['employmentStatus'] === 'pensioner'),
+    'personInfo.employmentStatus:unemployment' => checkbox($data['personInfo']['employmentStatus'] === 'unemployment'),
 ])
     ->needAppearances()
-    ->saveAs('filled.pdf')) {
+    ->send(preg_replace("/[\s]+/", '_', $name) . '.pdf')) {
     $error = $pdf->getError();
 
     echo '1' . $error;
+}
+
+
+function getName($data, $glue = ' ')
+{
+    return implode($glue, [$data['personInfo']['lastName'], $data['personInfo']['firstName'], $data['personInfo']['patronymic']]);
+}
+
+function getRole($data) {
+    return $data['personInfo']['role'] === 'borrower' ? 'Заёмщик' : 'Созаёмщик';
+}
+
+function checkbox($value) {
+    return $value ? 'Yes' : 'No';
 }
