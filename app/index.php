9fef23f5650ed94a5b27ece227c6484216a2dede
pinetree <kirill.sosnin@gmail.com>
2020-02-04 11:47:31 +0300

    Change dockerfiles

--- app/index.php
+++ app/index.php
@@ -1,2 +1,21 @@
 <?php
-phpinfo();
+require_once 'vendor/autoload.php';
+
+use mikehaertl\pdftk\Pdf;
+
+$pdf = new Pdf('/var/www/html/public/sample.pdf', [
+    'command' => '/usr/bin/pdftk',
+    'useExec' => true
+]);
+$pdf->tempDir = '/var/www/html/public/work';
+
+if (!$pdf->allow('AllFeatures')->fillForm([
+    'personInfo.lastName' => ('qqq1Соснин Кирилл2'),
+    'personInfo.nameChanged' => 'Yes',
+])
+    ->needAppearances()
+    ->saveAs('filled.pdf')) {
+    $error = $pdf->getError();
+
+    echo '1' . $error;
+}
